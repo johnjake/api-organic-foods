c5307b990f1f889f6d9a1d076181edb0611d947a
unknown <=>
2023-08-10 20:07:06 +0800

    playload request

---
+++ src/main/java/com/oranic/org/playload/request/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.oranic.org.playload.request;
+
+import com.oranic.org.model.roles.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
