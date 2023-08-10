14b5f8542092abd1b3700e18138b58a1190c49e1
unknown <=>
2023-08-10 19:11:00 +0800

    Access Denied Handler

---
+++ src/main/java/com/oranic/org/OrganicApplication.java
@@ -0,0 +1,13 @@
+package com.oranic.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/oranic/org/components/CustomAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package com.oranic.org.components;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    public static final Logger LOG = Logger.getLogger(String.valueOf(CustomAccessDeniedHandler.class));
+    
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            LOG.warning("User: " + auth.getName()
+                    + " attempted to access the protected URL: "
+                    + request.getRequestURI());
+        }
+
+        response.sendRedirect(request.getContextPath() + "/accessDenied");
+    }
+}
